c5531271b2719a226332d10aea1dcdb4cb838ca2
ShevchenkoO <shevchenko.sasha2005@gmail.com>
2025-07-16 20:42:28 +0200

    fix: redirect if 401

--- frontend/src/api/apartmentsApi.js
+++ frontend/src/api/apartmentsApi.js
@@ -3,31 +3,20 @@ const productApi = process.env.NEXT_PUBLIC_PRODUCTS_;
 export const getApartment = async (token) => {
     const res = await fetch(`${productApi}/apartment/`, {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-
-    if (res.status === 401) {
-        return null; // <-- возвращаем null при 401
-    }
-
-    if (!res.ok) {
-        return null; // или можно throw Error, зависит от твоей логики
-    }
-
-    return res.json();
-};
-
-export const getApartmentById = async (id, token) => {
-    const res = await fetch(`${productApi}/apartment/${id}/`, {
-        cache: 'no-store',
-        headers: {
             Authorization: `Bearer ${token}`,
         },
     });
 
-    if (res.status === 401 || !res.ok) {
-        return null;
+    if (res.status === 401) {
+        const error = new Error('Unauthorized');
+        error.status = 401;
+        throw error;
+    }
+
+    if (!res.ok) {
+        const error = new Error('Request failed');
+        error.status = res.status;
+        throw error;
     }
 
     return res.json();
